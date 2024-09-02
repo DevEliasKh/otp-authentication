e5d1471d39557355587ed45a14e66cb2c5eda671
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-09-02 20:25:07 +0330

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HomeViewVue from './views/HomeView.vue'
+</script>
+
+<template>
+  <HomeViewVue />
+</template>
+
+<style lang="scss">
+// just to fix vite bugs
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <main>elais</main>
+</template>
