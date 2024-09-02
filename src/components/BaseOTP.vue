60218201dd683af9d0683a7a970e16257bbdefa0
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2024-09-02 20:25:09 +0330

    fea: add OTP component

---
+++ src/components/BaseOTP.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="otp">
+    <div class="otp__title">کد تایید</div>
+    <div class="otp__boxes">
+      <template v-for="n in numberOfOTCBoxes" :key="n">
+        <BaseInput
+          type="text"
+          class="otp__box"
+          @input="onFilledInput"
+          :tabindex="n"
+          :autofocus="n == 1 ? true : false"
+          :value="inputDefault"
+          @paste="handlePaste"
+        />
+      </template>
+    </div>
+    <slot name="hint"> </slot>
+  </div>
+</template>
+<script setup>
+import BaseInput from '@/components/common/BaseInput.vue'
+
+defineProps({
+  onFilledInput: {
+    type: Function
+  },
+  inputDefault: {
+    type: Number || null
+  },
+  numberOfOTCBoxes: {
+    type: Number
+  }
+})
+</script>
+<style lang="scss"></style>
